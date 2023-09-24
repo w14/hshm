fbd158391d7a7cba77ddc4a00565fef5c2d58f5e
Will <w@Wills-MacBook-Pro.local>
2023-09-24 14:31:27 -0400

    new

---
+++ test.py
@@ -0,0 +1,20 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+import torch
+
+device = 'cuda'
+
+model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B")
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+input_text = "Miles Davis plays the"
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids.to(device)
+
+# Forward pass
+with torch.no_grad():
+    outputs = model(input_ids, output_attentions=True)
+
+# Get last layer, last time slot attention
+last_layer_attention = outputs.attentions[-1]  # Last layer
+last_time_slot_attention = last_layer_attention[0, :, -1, :]  # Last time slot
+
+# TODO: find correlation for specific "Miles Davis plays the" and compare against paper
